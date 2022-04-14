a9ef435931e6eedbda6f411e852c7c3acb7dc2bc
TA WDFT BCN Ironhack <ta-web-bcn@ironhack.com>
2022-04-14 12:22:13 +0200

    Create array_exercises.js

---
+++ array_exercises.js
@@ -0,0 +1,46 @@
+// --------ARRAYS--------
+
+// Exercise 1:
+// Write a function that takes as a first argument the number of times you want to repeat certain value and
+// the value you want to repeat as the second argument. Then it should return a new array with the given values.
+// Example:
+// fill(3, 'a'); // expected output => ['a', 'a', 'a']
+
+
+// Exercise 2:
+// Write a function that takes an array as the only argument and reverts it. Avoid using array.prototype.reverse().
+// Example:
+// reverse([1,2,3]); // expected output => [3,2,1]
+
+
+// Exercise 3:
+// Write a function that clears an array from all unnecessary elements (false, undefined, empty strings, 0, null).
+// Example:
+// compact([0, 1, false, 2, undefined, '', 3, null]); // expected output => [1,2,3]
+
+
+// Exercise 4:
+// Write a function that takes as an argument an array of arrays and returns an object composed of key-value pairs.
+// Example: 
+// formPairs([['a', 1],['b', 2]]); // expected output => { a: 1, b: 2}
+
+
+// Exercise 5:
+// Write a function that takes an array as a first argument and then two values as the second and third argument.
+// It should return an array without the two values passed as arguments.
+// Example:
+// without([1,2,3,1,2], 1, 2); // expected output => [3]
+
+
+// Exercise 6:
+// Write a function that takes two arrays as arguments and compares if they are identical.
+// Example: 
+// isEqual([1, 2, 3, 4],[1, 2, 3, 4]) // expected output => true
+// isEqual([1, 2, 3, 4],[1, 2, 3, 4, 5]) // expected output => false
+
+
+// Bonus – Exercise 7: 
+// Write a function that takes an array as the first argument and a number as the second argument.
+// It should return the array split into parts of the determined size.
+// Example:
+// chunk([1, 2, 3, 4, 5, 6, 7], 2); // expected output => [[1, 2], [3, 4], [5, 6], [7]]
